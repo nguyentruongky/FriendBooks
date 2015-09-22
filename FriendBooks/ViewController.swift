a53b9d8779847a04f971840f01aeaa97afcbea37
Nguyen Truong Ky <nguyentruongky3390@yahoo.com>
2015-09-22 11:56:02 +0700

    Initial Commit

---
+++ FriendBooks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FriendBooks
+//
+//  Created by Ky Nguyen on 9/22/15.
+//  Copyright © 2015 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
